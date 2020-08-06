8d1dcff0ff5c2ae565e4564638b3713b580dcb91
jackson-timoteo <jackson_siilva@hotmail.com>
2020-08-05 21:10:14 -0300

    add landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import logoImg from '../../assets/images/logo.svg';
+import landingImgm from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import '../Landing/style.css';
+
+
 function Landing(){
-    return  <h1>Landing</h1>
+    return  (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="logo-proffy"/>
+                    <h2>Sua plataforma de estudos online</h2>
+                </div>
+
+                <img src={landingImgm} alt="estudo" className="hero-image"/>
+
+                <div className="buttons-container">
+                    <a href="/" className="study">
+                        <img src={studyIcon} alt="estudar"/>
+                        Estudar
+                    </a>
+
+                    <a href="/" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas"/>
+                        Dar aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 coexões jpa realizadas <img src={purpleHeartIcon} alt="roxo"/>
+                </span>
+            </div>
+        </div>
+
+    )
 }
 
 export default Landing;
