4e505654a6afb45b13959a083cf3c70ea8d63759
jackson-timoteo <jackson_siilva@hotmail.com>
2020-08-15 23:11:38 -0300

    form proffy

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 
-import './styles.css'
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
 
 
 function TeacherForm(){
@@ -16,15 +19,30 @@ function TeacherForm(){
 
         <main>
             <fieldset>
-                <legend>Seus Dados</legend>
-
+                <legend>Seus Dados.</legend>
                 <Input name="name"  label="Nome conpleto" />
                 <Input name="avatar"  label="Avatar" />
                 <Input name="whatsapp"  label="whatsapp" />
-
-
-            
             </fieldset>
+
+            <fieldset>
+                <legend>Sonre a Aula.</legend>
+                <Input name="subject"  label="Matéria" />
+                <Input name="cost"  label="Custo da sua hora por Aula" />
+                <Input name="whatsapp"  label="whatsapp" />
+            </fieldset>
+
+            <footer>
+                <p>
+                    <img src={warningIcon} alt="Aviso Importante"/>
+                    Impartante! <br />
+                    Preencha todos os dados.
+                </p>
+                
+                <button>
+                    Salvar Cadastro
+                </button>
+            </footer>
         </main>
     </div>
     )
